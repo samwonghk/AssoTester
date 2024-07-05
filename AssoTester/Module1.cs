7454808a67732926f3d52357e703db036943b766
Sam Wong <sam.wong@aspatial.com.hk>
2024-07-05 09:47:41 +0100

    Add project files.

---
+++ AssoTester/Module1.cs
@@ -0,0 +1,101 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Data.UtilityNetwork;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Editing.Events;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AssoTester
+{
+    internal class Module1 : Module
+    {
+        private static Module1 _this = null;
+
+        /// <summary>
+        /// Retrieve the singleton instance to this module here
+        /// </summary>
+        public static Module1 Current => _this ??= (Module1)FrameworkApplication.FindModule("AssoTester_Module");
+
+        #region Overrides
+        /// <summary>
+        /// Called by Framework when ArcGIS Pro is closing
+        /// </summary>
+        /// <returns>False to prevent Pro from closing, otherwise True</returns>
+        protected override bool CanUnload()
+        {
+            //TODO - add your business logic
+            //return false to ~cancel~ Application close
+            return true;
+        }
+
+        #endregion Overrides
+
+        protected override bool Initialize()
+        {
+            //var mapItem = Project.Current.GetItems<MapProjectItem>().FirstOrDefault(x => x.Name == "AMFM Editor");
+            //QueuedTask.Run(() =>
+            //{
+            //    var map = mapItem.GetMap();
+
+            //    var layers = map.GetLayersAsFlattenedList().OfType<FeatureLayer>();
+            //    foreach (var layer in layers)
+            //    {
+            //        if (layer is FeatureLayer)
+            //        {
+            //            var bfLayer = (FeatureLayer)layer;
+            //            var table = bfLayer.GetTable();
+            //            if (bfLayer.Name == "Stormwater Culvert Path Boundary") RowDeletedEvent.Subscribe(OnRowDeleted, table);
+            //        }
+            //    }
+            //});
+            //EditCompletedEvent.Subscribe(OnEditCompleted);
+            return base.Initialize();
+        }
+        
+        private Task OnEditCompleted(EditCompletedEventArgs args)
+        {
+            
+            return Task.CompletedTask;
+        }
+
+        private void OnRowDeleted(RowChangedEventArgs args)
+        {
+            
+            var featureClass = args.Row.GetTable() as FeatureClass;
+            if (featureClass is not null)
+            {
+                var ctrlDatasets = featureClass.GetControllerDatasets();
+                foreach (var ctrlDataset in ctrlDatasets)
+                {
+                    if (ctrlDataset is UtilityNetwork un)
+                    {
+                        var currentFeature = args.Row;
+                        var element = un.CreateElement(currentFeature);
+                        var assocs = un.GetAssociations(element);
+                        foreach (var assoc in assocs)
+                        {
+                            un.DeleteAssociation(assoc);
+                        }
+                                    
+                    }
+                }
+            }
+        }
+            
+    }
+}
---
+++ AssoTester/Tools/DeleteWithAsso.cs
@@ -0,0 +1,91 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Data.UtilityNetwork;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Editing.Events;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssoTester.Tools
+{
+    internal class DeleteWithAsso : Button
+    {
+        protected override void OnClick()
+        {
+            QueuedTask.Run(() => {
+                var selections = MapView.Active.Map.GetSelection();
+                foreach (var selection in selections.ToDictionary())
+                {
+                    if (selection.Key is FeatureLayer featureLayer)
+                    {
+                        var featureClass = featureLayer.GetFeatureClass();
+                        var eventToken = RowDeletedEvent.Subscribe(OnRowDeleted, featureClass);
+
+                        if (featureClass is not null)
+                        {
+                            var ctrlDatasets = featureClass.GetControllerDatasets();
+                            foreach (var ctrlDataset in ctrlDatasets)
+                            {
+                                if (ctrlDataset is UtilityNetwork un)
+                                {
+                                    EditOperation editOperation = new EditOperation();
+                                    editOperation.Name = "Delete with Associations";
+                                    editOperation.EditOperationType = EditOperationType.Long;
+                                    var qf = new QueryFilter() { ObjectIDs = selection.Value };
+                                    var rows = featureLayer.Search(qf);
+
+                                    while (rows.MoveNext())
+                                    {
+                                        var currentFeature = rows.Current;
+                                        editOperation.Delete(currentFeature);
+                                    }
+
+                                    var result = editOperation.Execute();
+                                    MessageBox.Show(result.ToString());
+                                    if (!result) MessageBox.Show(editOperation.ErrorMessage);
+                                }
+                            }
+                        }
+                        RowDeletedEvent.Unsubscribe(eventToken);
+                    }
+                }
+            });
+        }
+        private void OnRowDeleted(RowChangedEventArgs args)
+        {
+
+            var featureClass = args.Row.GetTable() as FeatureClass;
+            if (featureClass is not null)
+            {
+                var ctrlDatasets = featureClass.GetControllerDatasets();
+                foreach (var ctrlDataset in ctrlDatasets)
+                {
+                    if (ctrlDataset is UtilityNetwork un)
+                    {
+                        var currentFeature = args.Row;
+                        var element = un.CreateElement(currentFeature);
+                        var assocs = un.GetAssociations(element);
+                        foreach (var assoc in assocs)
+                        {
+                            un.DeleteAssociation(assoc);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
